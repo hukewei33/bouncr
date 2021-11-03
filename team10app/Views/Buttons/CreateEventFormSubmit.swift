4644eee7f65faf2276ae2c7c76cda01fc8fc5cfe
Sara Song <shsong@andrew.cmu.edu>
2021-11-02 23:38:36 -0400

    Added blank form fields (no data involved) to create event view, created a new SwiftUI file for form submit button

---
+++ team10app/Views/Buttons/CreateEventFormSubmit.swift
@@ -0,0 +1,15 @@
+//
+//  CreateEventFormSubmit.swift
+//  team10app
+//
+//  Created by Sara Song on 11/2/21.
+//
+
+import SwiftUI
+
+struct CreateEventFormSubmit: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
