6032039949eaebc43352d760eb1fa12df31fb09e
Sara Song <shsong@andrew.cmu.edu>
2021-11-05 01:53:27 -0400

    Created new file for edit event view

---
+++ team10app/Views/Host Views/EditEventView.swift
@@ -0,0 +1,15 @@
+//
+//  EditEventView.swift
+//  team10app
+//
+//  Created by Sara Song on 11/5/21.
+//
+
+import SwiftUI
+
+struct EditEventView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
