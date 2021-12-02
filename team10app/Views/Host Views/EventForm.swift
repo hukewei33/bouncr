420705e9ed0387eb94f49fe0bc4dfc3bc1cdfdcb
Sara Song <shsong@andrew.cmu.edu>
2021-12-02 03:17:36 -0500

    Created new file to combine create and edit event forms

---
+++ team10app/Views/Host Views/EventForm.swift
@@ -0,0 +1,20 @@
+//
+//  EventForm.swift
+//  team10app
+//
+//  Created by Sara Song on 12/2/21.
+//
+
+import SwiftUI
+
+struct EventForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EventForm_Previews: PreviewProvider {
+    static var previews: some View {
+        EventForm()
+    }
+}
