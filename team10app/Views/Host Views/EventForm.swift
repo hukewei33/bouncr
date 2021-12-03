ed7f5bb45331ce0e02feda986bd56c648d750595
grace joseph <gejoseph@andrew.cmu.edu>
2021-12-03 09:06:06 -0500

    adds events

--- team10app/Views/Host Views/EventForm.swift
+++ team10app/Views/Host Views/EventForm.swift
@@ -21,6 +21,11 @@ struct EventForm: View {
   var navTitle: String
   @Environment(\.presentationMode) var mode: Binding<PresentationMode>
   
+//  init(viewModel: ViewModel, navTitle: String){
+//    self.viewModel = viewModel
+//    self.navTitle = navTitle
+//  }
+  
   @State private var name: String = ""
   @State private var startTime = Date()
   @State private var endTime = Date()
