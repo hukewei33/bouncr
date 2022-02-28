e3caa7a878301e18d50043c11557da50c4c5b20f
kenny hu <hukewei33@gmail.com>
2022-02-28 12:01:10 -0500

    small change to user model

--- railsServer/bouncr/app/models/user.rb
+++ railsServer/bouncr/app/models/user.rb
@@ -5,8 +5,7 @@ class User < ApplicationRecord
     has_many :invites, dependent: :destroy
     has_many :events, through: :hosts
     has_many :events, through: :invites
-    has_many :initiatedFriendship, class_name: 'Friend', 
-    foreign_key: 'user1_id'
-    has_many :recievedFriendship, class_name: 'Friend',
-        foreign_key: 'user2_id'
+
+    has_many :initiatedFriendship, class_name: 'Friend', foreign_key: 'user1_id'
+    has_many :recievedFriendship, class_name: 'Friend', foreign_key: 'user2_id'
 end
