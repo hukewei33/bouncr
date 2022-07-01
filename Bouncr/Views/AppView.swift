edb8196b889a36b5e9ba998d23fd508ce3f89480
Sara Song <shsong@andrew.cmu.edu>
2022-06-30 22:26:14 -0400

    Added basic UI files

---
+++ Bouncr/Views/AppView.swift
@@ -0,0 +1,15 @@
+//
+//  AppView.swift
+//  Bouncr
+//
+//  Created by Sara Song on 6/30/22.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
