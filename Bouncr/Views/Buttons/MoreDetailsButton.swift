b8c6f34f48e99aec68444524e258ab213254c121
Sara Song <shsong@andrew.cmu.edu>
2022-07-06 01:03:09 -0400

    copied over qr button

---
+++ Bouncr/Views/Buttons/MoreDetailsButton.swift
@@ -0,0 +1,20 @@
+//
+//  MoreDetailsButton.swift
+//  Bouncr
+//
+//  Created by Sara Song on 7/6/22.
+//
+
+import SwiftUI
+
+struct MoreDetailsButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MoreDetailsButton_Previews: PreviewProvider {
+    static var previews: some View {
+        MoreDetailsButton()
+    }
+}
